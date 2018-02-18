027fe9f84a82f39c13cebf5a6000fc1caad7861b
mcquilla <dorothy@DorothyComputer.cable.rcn.com>
2018-02-17 23:35:43 -0500

    initial commit - frontend, backend, proto - all builds with bazel

---
+++ go/src/ml_server/backend/backend.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"ml_server/proto/learner"
+	"net"
+)
+
+const (
+	port = ":80"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) Regress(ctx context.Context, in *learner.RegressRequest) (*learner.RegressResponse, error) {
+	return &learner.RegressResponse{Prediction: 9.99}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	learner.RegisterLearnerServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ go/src/ml_server/frontend/frontend.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	"ml_server/proto/learner"
+	"net/http"
+)
+
+const (
+	address = "backend:80"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	test := &learner.RegressRequest{
+		Train: &learner.Matrix{
+			DataPoint: []*learner.DataPoint{
+				{
+					Obs:  8.1,
+					Vars: []float32{10294, 841, 10},
+				}, {
+					Obs:  8.1,
+					Vars: []float32{10294, 841, 10},
+				}, {
+					Obs:  8.1,
+					Vars: []float32{10294, 841, 10},
+				},
+			},
+		},
+	}
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := learner.NewLearnerClient(conn)
+
+	resp, err := c.Regress(context.Background(), test)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	fmt.Fprintf(w, "Prediction: %s", resp.Prediction)
+
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":12827", nil)
+}
